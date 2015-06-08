5d149168362a19df052697253874baaef6f5b1c9
leonard-lin <linle.job@gmail.com>
2015-06-08 16:18:34 -0700

    updates

---
+++ MaterialDesign/navigationdrawer/src/main/java/androidpath/ll/navigationdrawer/NavigationDrawerFragment.java
@@ -0,0 +1,106 @@
+package androidpath.ll.navigationdrawer;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    private static final String PREF_FILE_NAME = "testpref";
+    public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+
+    private ActionBarDrawerToggle mDrawerToogle;
+    private DrawerLayout mDrawerLayout;
+    private View containerView;
+
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstanceState;
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserLearnedDrawer = readFromPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, false); //user never open drawer before
+        mFromSavedInstanceState = savedInstanceState != null ? true : false;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
+
+        containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToogle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                if (slideOffset < 0.6) {
+                    toolbar.setAlpha(1 - slideOffset);
+                }
+
+
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToogle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToogle.syncState();
+                if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
+                    mDrawerLayout.openDrawer(containerView);
+                    mUserLearnedDrawer = true;
+                    saveToPreferences(getActivity(), KEY_USER_LEARNED_DRAWER, mUserLearnedDrawer);
+                }
+            }
+        });
+
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, boolean preferenceValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static boolean readFromPreferences(Context context, String preferenceName, boolean defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        return sharedPreferences.getBoolean(preferenceName, defaultValue);
+    }
+
+}
---
+++ RottenTomatoes/app/src/main/java/androidpath/ll/rottentomatoes/MainActivity.java
@@ -0,0 +1,78 @@
+package androidpath.ll.rottentomatoes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String MOVIE_DETAIL_KEY = "movie";
+
+    private ListView lvMovies;
+    private BoxOfficeMoviesAdapter adapterMovies;
+    RottenTomatoesClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvMovies = (ListView) findViewById(R.id.lvMovies);
+
+        fetchBoxOfficeMovies();
+
+        ArrayList<BoxOfficeMovie> aMovies = new ArrayList<BoxOfficeMovie>();
+        adapterMovies = new BoxOfficeMoviesAdapter(this, aMovies);
+        lvMovies.setAdapter(adapterMovies);
+        setupMovieSelectedListener();
+    }
+
+
+    private void fetchBoxOfficeMovies() {
+        client = new RottenTomatoesClient();
+        client.getBoxOfficeMovies(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
+                JSONArray items = null;
+                try {
+                    // Get the movies json array
+                    items = responseBody.getJSONArray("movies");
+                    // Parse json array into array of model objects
+                    ArrayList<BoxOfficeMovie> movies = BoxOfficeMovie.fromJson(items);
+                    // Load model objects into the adapter
+                    for (BoxOfficeMovie movie : movies) {
+                        adapterMovies.add(movie); // add movie through the adapter
+                    }
+                    adapterMovies.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void setupMovieSelectedListener() {
+        lvMovies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View item, int position, long rowId) {
+                // Launch the detail view passing movie as an extra
+                Intent i = new Intent(MainActivity.this, BoxOfficeDetailActivity.class);
+                i.putExtra(MOVIE_DETAIL_KEY, adapterMovies.getItem(position));
+                startActivity(i);
+            }
+        });
+    }
+}
