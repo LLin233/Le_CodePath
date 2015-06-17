e96e24678de07efb10f1b4f63669346bc6d5a09f
leonard-lin <linle.job@gmail.com>
2015-06-16 18:46:40 -0700

    send http request via Volley

--- MaterialDesign/material/src/main/java/androidpath/ll/material/ui/PageFragment.java
+++ MaterialDesign/material/src/main/java/androidpath/ll/material/ui/PageFragment.java
@@ -6,14 +6,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 import androidpath.ll.material.R;
+import androidpath.ll.material.network.VolleySingleton;
 
 /**
  * Created by Le on 2015/6/14.
  */
 public class PageFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
+    final String URL = "http://php.net/";
     private int mPage;
 
     public static PageFragment getInstence(int position) {
@@ -36,6 +45,22 @@ public class PageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_page, container, false);
         TextView textView = (TextView) view.findViewById(R.id.tv);
         textView.setText("Fragment #" + mPage);
+
+        //RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        RequestQueue requestQueue = VolleySingleton.getInstance().getRequestQueue();
+        StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getActivity(), "RESPONSE" + response, Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "Error" + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(request);
+
         return view;
 
     }
