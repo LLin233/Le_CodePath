0aba6dd1bdb6a433a4f4b1a43e40c3275f769c75
leonard-lin <linle.job@gmail.com>
2015-06-25 16:47:13 -0700

    created MovieSorter class to handle sorting operation.

---
+++ MaterialDesign/material/src/main/java/androidpath/ll/material/Utils/MovieSorter.java
@@ -0,0 +1,40 @@
+package androidpath.ll.material.Utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import androidpath.ll.material.Models.Movie;
+
+/**
+ * Created by Le on 2015/6/25.
+ */
+public class MovieSorter {
+    public void sortMoviesByName(ArrayList<Movie> movies) {
+        Collections.sort(movies, new Comparator<Movie>() {
+            @Override
+            public int compare(Movie lhs, Movie rhs) {
+                return lhs.getTitle().compareTo(rhs.getTitle());
+            }
+        });
+    }
+
+    public void sortMoviesByDate(ArrayList<Movie> movies) {
+        Collections.sort(movies, new Comparator<Movie>() {
+            @Override
+            public int compare(Movie lhs, Movie rhs) {
+                return lhs.getReleaseDateTheater().compareTo(rhs.getReleaseDateTheater());
+            }
+        });
+    }
+
+    public void sortMoviesByRating(ArrayList<Movie> movies) {
+        Collections.sort(movies, new Comparator<Movie>() {
+            @Override
+            public int compare(Movie lhs, Movie rhs) {
+                return rhs.getAudienceScore() - lhs.getAudienceScore();
+            }
+        });
+    }
+
+}
