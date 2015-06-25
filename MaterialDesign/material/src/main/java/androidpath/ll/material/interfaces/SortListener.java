05f653e5c6ba89b268a4d487734dfccbba0cbf59
leonard-lin <linle.job@gmail.com>
2015-06-25 16:46:14 -0700

    moved listener under the right package name

---
+++ MaterialDesign/material/src/main/java/androidpath/ll/material/interfaces/SortListener.java
@@ -0,0 +1,10 @@
+package androidpath.ll.material.interfaces;
+
+/**
+ * Created by Le on 2015/6/25.
+ */
+public interface SortListener {
+    public void onSortByName();
+    public void onSortByDate();
+    public void onSortByRating();
+}
