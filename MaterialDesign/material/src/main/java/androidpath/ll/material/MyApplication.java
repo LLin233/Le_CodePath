5f6e94c9fb4f4c926fb0ac84a38018e53a4c7eb0
leonard-lin <linle.job@gmail.com>
2015-06-29 01:12:22 -0700

    Added ActiveAndroid config code

--- MaterialDesign/material/src/main/java/androidpath/ll/material/MyApplication.java
+++ MaterialDesign/material/src/main/java/androidpath/ll/material/MyApplication.java
@@ -3,15 +3,25 @@ package androidpath.ll.material;
 import android.app.Application;
 import android.content.Context;
 
+import com.activeandroid.ActiveAndroid;
+
 /**
  * Created by Le on 2015/6/15.
  */
 public class MyApplication extends Application {
     private static MyApplication mInstance;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        ActiveAndroid.initialize(this);
         mInstance = this;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ActiveAndroid.dispose();
     }
 
     public static Context getAppContext() {
