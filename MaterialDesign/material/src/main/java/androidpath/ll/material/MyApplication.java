3536b4bf04b963b03e16c694a155fd468211d346
leonard-lin <linle.job@gmail.com>
2015-07-02 09:01:51 -0700

    added LeakCanary to detect possible memory leak. {@link {https://github.com/square/leakcanary}}

--- MaterialDesign/material/src/main/java/androidpath/ll/material/MyApplication.java
+++ MaterialDesign/material/src/main/java/androidpath/ll/material/MyApplication.java
@@ -4,18 +4,22 @@ import android.app.Application;
 import android.content.Context;
 
 import com.activeandroid.ActiveAndroid;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 /**
  * Created by Le on 2015/6/15.
  */
 public class MyApplication extends Application {
     private static MyApplication mInstance;
+    private RefWatcher refWatcher;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        ActiveAndroid.initialize(this);
         mInstance = this;
+        ActiveAndroid.initialize(this);
+        refWatcher = LeakCanary.install(this);
     }
 
     @Override
@@ -27,4 +31,12 @@ public class MyApplication extends Application {
     public static Context getAppContext() {
         return mInstance.getApplicationContext();
     }
+
+
+    public static RefWatcher getRefWatcher(Context context) {
+        MyApplication application = (MyApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+
 }
